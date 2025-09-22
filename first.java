1e3f87cc6489b5ff7db45316760e44f37a3eab91
Raphael Gstettenhofer <umbraadmortem8@gmail.com>
2025-09-22 14:18:28 +0200

    Add dollar to euro conversion

--- first.java
+++ first.java
@@ -2,5 +2,11 @@ class First {
   public static void main() {
     System.out.println("Hello World");
     System.out.println("Vorkurs: 2025");
+
+    double dollar_to_euro = 0.85;
+    double dollar = 100;
+    double euro = dollar * dollar_to_euro;
+
+    System.out.println(dollar + " $ are " + euro + " €");
   }
 }
