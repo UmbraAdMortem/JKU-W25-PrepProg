b0a99b96e640d0fe577d05d1878a3ca23d9c6178
Raphael Gstettenhofer <umbraadmortem8@gmail.com>
2025-09-23 10:27:28 +0200

    Add convenience functions for triangles

--- pattern.java
+++ pattern.java
@@ -1,17 +1,17 @@
 public class pattern {
     public static void main() {
 
-        // triangle(3);
-        parallel(3, 3, 1, -2);
+        triangle(3);
+        // parallel(3, 3, 1, -2);
         System.out.println();
-        
+
         parallel(6, 5, 2, 0);
         System.out.println();
-        
-        // triangle(4);
-        parallel(4, 4, 1, -3);
-        // triangleReverse(3);
-        parallel(3, 3, -1, 0);
+
+        triangle(4);
+        // parallel(4, 4, 1, -3);
+        triangleReverse(3);
+        // parallel(3, 3, -1, 0);
         System.out.println();
 
         parallel(4, 4, -1, 4);
@@ -19,21 +19,25 @@ public class pattern {
     }
 
     public static void triangle(int size) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print('#');
-            }
-            System.out.println();
-        }
+        parallel(size, size, 1, 1 - size);
+
+        // for (int i = 0; i < size; i++) {
+        // for (int j = 0; j <= i; j++) {
+        // System.out.print('#');
+        // }
+        // System.out.println();
+        // }
     }
 
     public static void triangleReverse(int size) {
-        for (int i = size; i > 0; i--) {
-            for (int j = i; j > 0; j--) {
-                System.out.print('#');
-            }
-            System.out.println();
-        }
+        parallel(size, size, -1, 0);
+
+        // for (int i = size; i > 0; i--) {
+        // for (int j = i; j > 0; j--) {
+        // System.out.print('#');
+        // }
+        // System.out.println();
+        // }
     }
 
     public static void parallel(int width, int height, int indent, int initIndent) {
