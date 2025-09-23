0126a4638422df6c2efc6b01cf65c2c12b85688f
Raphael Gstettenhofer <umbraadmortem8@gmail.com>
2025-09-23 13:38:12 +0200

    Add absolute function

--- methods.java
+++ methods.java
@@ -11,6 +11,10 @@ public class methods {
         return number * number;
     }
 
+    public static double abs(double x) {
+        return x < 0 ? -x : x;
+    }
+
     public static void main() {
         for (double x = 0.0; x < 10; ++x) {
             System.out.println("f(" + x + ") = " + f(x));
