2094f3e237dca0788cf55170e8ef3261d643aae6
Raphael Gstettenhofer <umbraadmortem8@gmail.com>
2025-09-23 10:06:32 +0200

    Add parallel

--- pattern.java
+++ pattern.java
@@ -3,12 +3,25 @@ public class pattern {
 
         triangle(3);
         System.out.println();
+
+        parallel(6, 5, 2);
+        System.out.println();
     }
 
     public static void triangle(int size) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print('#');
+            }
+            System.out.println();
+        }
+    }
+
+    public static void parallel(int width, int height, int indent) {
+        for (int i = 0; i < height; i++) {
+            int indentation = i * indent;
+            for (int j = 0; j < width + indentation; j++) {
+                System.out.print(j < indentation ? ' ' : '#');
             }
             System.out.println();
         }
