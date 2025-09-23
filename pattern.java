4bba5777d83eeb988cafaf119183cc26deef339d
Raphael Gstettenhofer <umbraadmortem8@gmail.com>
2025-09-23 10:06:51 +0200

    Add triangle reverse

--- pattern.java
+++ pattern.java
@@ -6,11 +6,27 @@ public class pattern {
 
         parallel(6, 5, 2);
         System.out.println();
+        
+        triangle(4);
+        triangleReverse(3);
+        System.out.println();
+
+        
     }
 
     public static void triangle(int size) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j <= i; j++) {
+                System.out.print('#');
+            }
+            System.out.println();
+        }
+    }
+
+    public static void triangleReverse(int size)
+    {
+        for (int i = size; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
                 System.out.print('#');
             }
             System.out.println();
