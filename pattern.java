d10666793823fa28439066014a5a48e56378086e
Raphael Gstettenhofer <umbraadmortem8@gmail.com>
2025-09-23 10:06:02 +0200

    Refactor triangle

--- pattern.java
+++ pattern.java
@@ -1,7 +1,11 @@
 public class pattern {
     public static void main() {
-        int size = 3;
 
+        triangle(3);
+        System.out.println();
+    }
+
+    public static void triangle(int size) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print('#');
