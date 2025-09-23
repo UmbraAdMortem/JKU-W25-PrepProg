00ee94897c5c83adec646c84193e681c72cc6876
Raphael Gstettenhofer <umbraadmortem8@gmail.com>
2025-09-23 16:15:31 +0200

    Use while

--- DigitSum.java
+++ DigitSum.java
@@ -1,14 +1,22 @@
 public class DigitSum {
     public static int digitSum(int number) {
-        if (number <= 0)
-            return 0;
-        return number % 10 + digitSum(number / 10);
+        int sum = 0;
+        while(number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+
+        // if (number <= 0)
+        //     return 0;
+        // return number % 10 + digitSum(number / 10);
     }
 
     public static void main() {
         int number = 123;
         int sum = digitSum(number);
-
+        System.out.print("Test");
         System.out.println("DigitSum(" + number + ") = " + sum);
     }
 }
