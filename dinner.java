c518ec726702019d53b1aee9a5357f9d1d8fed52
Raphael Gstettenhofer <umbraadmortem8@gmail.com>
2025-09-23 08:19:57 +0200

    Use HashMap for storing ingrediences

--- dinner.java
+++ dinner.java
@@ -1,62 +1,43 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+// Nudelmenge
+// Wassermenge
+// Parmesanmenge
+// Salz
+// Sugo
+// Basilikum
+
+// Ausgabe: Die Mengen
+// Preis
+
 public class dinner {
     public static void main() {
-        double persons = 4;
+        Double persons = 4.0;
+        Double pasta = 120.0; // g / person
 
-        double pasta = 120.0; // g / persion
-        double bacon = 50.0; // g / persion
-        double eggs = 1; // g / persion
-        double parmesan = 20.0; // g / persion
-        double salz = 7.0; // g / persion
-        double water = 300.0; // ml / 100g
+        HashMap<String, Double[]> ingredient = new HashMap<String, Double[]>();
 
-        pasta = pasta * persons;
-        bacon = bacon * persons;
-        eggs = eggs * persons;
-        parmesan = parmesan * persons;
-        salz = salz * persons;
-        water = pasta / 100.0 * water;
+        ingredient.put("Pasta", new Double[] { 120.0, 1.98 / 1000.0 }); // g / person | € / g
+        ingredient.put("Bacon", new Double[] { 50.0, 47.67 / 1000.0 }); // g / person | € / g
+        ingredient.put("Eggs", new Double[] { 1.0, 0.48 }); // g / person | € / g
+        ingredient.put("Parmesan", new Double[] { 20.0, 42.25 / 1000.0 }); // g / person | € / g
+        ingredient.put("Salt", new Double[] { 7.0, 1.38 / 1000.0 }); // g / person | € / g
+        ingredient.put("Water", new Double[] { 300.0 * pasta / 100.0, 0.18 / 1000.0 }); // ml / 100g | € / ml
 
         System.out.println("For Spaghetti Cabonara for " + persons + " people you need:");
-        System.out.println(" * pasta: " + pasta + " g");
-        System.out.println(" * bacon: " + bacon + " g");
-        System.out.println(" * eggs: " + eggs);
-        System.out.println(" * parmesan: " + parmesan + " g");
-        System.out.println(" * salz: " + salz + " g");
-        System.out.println(" * water: " + water + " ml");
 
-        double p_pasta = 1.98 / 1000.0; // € / g
-        double p_bacon = 47.67 / 1000.0; // € / g
-        double p_eggs = 0.48; // € / a
-        double p_parmesan = 42.25 / 1000.0; // € / g
-        double p_salz = 1.38 / 1000.0; // € / g
-        double p_water = 0.18 / 1000.0; // € / ml
+        Double prize = 0.0;
+        Iterator<HashMap.Entry<String, Double[]>> it = ingredient.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, Double[]> entry = it.next();
+            Double amount = entry.getValue()[0] * persons;
+            prize += amount * entry.getValue()[1];
 
-        p_pasta = p_pasta * pasta;
-        p_bacon = p_bacon * bacon;
-        p_eggs = p_eggs * eggs;
-        p_parmesan = p_parmesan * parmesan;
-        p_salz = p_salz * salz;
-        p_water = p_water * water;
+            System.out.println(String.format(" * %-10s: %10.2f g", entry.getKey(), amount));
+        }
 
-        double expanse = p_pasta +
-                            p_bacon +
-                            p_eggs +
-                            p_parmesan +
-                            p_salz +
-                            p_water;
-
-        String text = String.format("The meal costs: %.2f € for %d people", expanse, (int)persons);
-        System.out.println(text);
-
-
-        // Nudelmenge
-        // Wassermenge
-        // Parmesanmenge
-        // Salz
-        // Sugo
-        // Basilikum
-
-        // Ausgabe: Die Mengen
-        // Preis
+        System.out.println(String.format("The meal costs: %.2f € for %.0f people", prize, persons));
     }
 }
