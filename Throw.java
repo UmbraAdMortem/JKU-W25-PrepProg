a139e645f20bdfebb5e0182cefb7688908187d93
Raphael Gstettenhofer <umbraadmortem8@gmail.com>
2025-09-24 11:05:37 +0200

    Add rebound

--- Throw.java
+++ Throw.java
@@ -1,8 +1,12 @@
 public class Throw {
     public static double queryDouble(String output) {
         System.out.print(output);
-        double input = In.readDouble();
-        return input;
+        return In.readDouble();
+    }
+
+    public static int queryInt(String output) {
+        System.out.print(output);
+        return In.readInt();
     }
 
     public static void main(String[] args) {
@@ -12,6 +16,7 @@ public class Throw {
         double velocity_y = 1.0;
         double gravity_x = 0.0;
         double gravity_y = -0.1;
+        int time_max = 120;
 
         // double pos_x = queryDouble("Provide initial position in x: ");
         // double pos_y = queryDouble("Provide initial position in y: ");
@@ -19,13 +24,15 @@ public class Throw {
         // double velocity_y = queryDouble("Provide initial velovity in y: ");
         // double gravity_x = 0.0;
         // double gravity_y = queryDouble("Provide initial gravity in y: ");
+        // int time_max = queryInt("Provide maximum abount of simulation time: ");
         // System.out.println();
+
 
         System.out.println(
                 "--------------------------------------------------------------------------------------------> Y");
 
         int time = 0;
-        for (; time <= 10000 && pos_y >= 0.0; ++time) {
+        for (; time <= time_max; ++time) {
             System.out.format("Time %4ds: P(%5.2f|%5.2f) |", time, pos_x, pos_y);
             for (int y = 0; y < (int) (pos_y * 10); ++y)
                 System.out.print(" ");
@@ -36,6 +43,10 @@ public class Throw {
 
             pos_x += velocity_x;
             pos_y += velocity_y;
+
+            if (pos_y <= 0.1)
+                velocity_y = -velocity_y;
+
             velocity_x += gravity_x;
             velocity_y += gravity_y;
         }
