82bff13f7e3edc6debd88408822dab198aa3e7e5
Raphael Gstettenhofer <umbraadmortem8@gmail.com>
2025-09-24 10:08:25 +0200

    Add input to throw calculation

--- Throw.java
+++ Throw.java
@@ -2,27 +2,26 @@ public class Throw {
     public static double queryDouble(String output) {
         System.out.print(output);
         double input = In.readDouble();
-        System.out.println();
         return input;
     }
-
+    
     public static void main(String[] args) {
-        double pos_x = 0.0;
-        double pos_y = 0.0;
-        double velocity_x = 1.0;
-        double velocity_y = 1.0;
-        double gravity_x = 0.0;
-        double gravity_y = -0.1;
-
-        // double pos_x = queryDouble("Provide initial position in x: ");
-        // double pos_y = queryDouble("Provide initial position in y: ");
-        // double velocity_x = queryDouble("Provide initial velovity in x: ");
-        // double velocity_y = queryDouble("Provide initial velovity in y: ");
+        // double pos_x = 0.0;
+        // double pos_y = 0.0;
+        // double velocity_x = 1.0;
+        // double velocity_y = 1.0;
         // double gravity_x = 0.0;
-        // // double gravity_x = queryDouble("Provide initial gravity in x: ");
-        // double gravity_y = queryDouble("Provide initial gravity in y: ");
+        // double gravity_y = -0.1;
 
-        for (int time = 0; pos_y >= 0.0; ++time) {
+        double pos_x = queryDouble("Provide initial position in x: ");
+        double pos_y = queryDouble("Provide initial position in y: ");
+        double velocity_x = queryDouble("Provide initial velovity in x: ");
+        double velocity_y = queryDouble("Provide initial velovity in y: ");
+        double gravity_x = 0.0;
+        double gravity_y = queryDouble("Provide initial gravity in y: ");
+        System.out.println();
+
+        for (int time = 0; time <= 10000 && pos_y >= 0.0; ++time) {
             pos_x += velocity_x;
             pos_y += velocity_y;
             velocity_x += gravity_x;
