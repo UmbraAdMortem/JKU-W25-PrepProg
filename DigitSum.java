5c51af5be280e2fdb779e282b3d2ed472a243162
Raphael Gstettenhofer <umbraadmortem8@gmail.com>
2025-09-24 09:17:57 +0200

    Add input to DigitSum

--- DigitSum.java
+++ DigitSum.java
@@ -1,22 +1,28 @@
 public class DigitSum {
     public static int digitSum(int number) {
         int sum = 0;
-        while(number > 0)
-        {
+        while (number > 0) {
             sum += number % 10;
             number /= 10;
         }
         return sum;
 
         // if (number <= 0)
-        //     return 0;
+        // return 0;
         // return number % 10 + digitSum(number / 10);
     }
 
     public static void main() {
-        int number = 123;
-        int sum = digitSum(number);
-        System.out.print("Test");
-        System.out.println("DigitSum(" + number + ") = " + sum);
+
+        while (true) {
+            System.out.print("Please enter number greater than 0: ");
+            int number = In.readInt();
+            System.out.println();
+            if (number <= 0)
+                return;
+
+            int sum = digitSum(number);
+            System.out.println("DigitSum(" + number + ") = " + sum);
+        }
     }
 }
